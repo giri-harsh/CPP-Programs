c7faf7c034714578ab631333043431f4249c08d4
Harsh Giri <2006.harshgiri@gmail.com>
2024-07-09 22:21:48 +0530

    Program 22

---
+++ To remove contents of a string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string str = "Hello world";
+    cout<<str<<endl;
+
+
+cout<<"To remove content of the string "<<endl;;
+    str.clear();
+    cout<<"output should be blank"<<str;
+  
+}
