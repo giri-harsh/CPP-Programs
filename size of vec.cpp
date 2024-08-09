ff9bf703201cad1ce325defea8cdeeb710615ab1
Harsh Giri <2006.harshgiri@gmail.com>
2024-08-09 09:23:12 +0530

    Program 48

---
+++ size of vec.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector <int> v;
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    cout<<"size is : "<<v.size()<<endl;
+    for (int i = 0;i<v.size();i++){
+        cout<<v[i]<<endl;
+    }
+}
