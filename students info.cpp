984fae6f0df620f522daafd3f5a0b262e6b80f77
giri-harsh <2006.harshgiri@gmail.com>
2024-08-14 09:30:10 +0530

    program 51

---
+++ students info.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class student{
+    public :
+    string name;
+    int age;
+    bool gender;
+
+    void print(){
+        cout<<"name ="<<name<<endl;
+        cout<<"age = "<<age<<endl;
+        cout<<"gender = "<<gender<<endl;
+    }
+
+};
+
+
+int main(){
+    student arr[3];
+    for (int i =0;i<3;i++){
+        cout<<"enter name "<<endl;
+        cin>>arr[i].name;
+        cout<<"enter age "<<endl;
+        cin>>arr[i].age;
+        cout<<"enter gender "<<endl;
+        cin>>arr[i].gender;
+
+    }
+    for (int i =0;i<3;i++){
+        arr[i].print();
+    }
+
+
+
+}
